29a2c83752a76887a9838612d0f189b1e4948805
Brigham Andersen <brighambandersen@gmail.com>
2026-06-05 23:53:15 -0600

    deploy ~/dev changes

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -5,12 +5,12 @@ echo "Deploying groceries"
 
 # frontend
 
-cd /home/brig/code/groceries/
+cd /home/brig/dev/groceries/
 npm run build
 
 # nginx
 
-sudo cp /home/brig/code/groceries/deploy/nginx.conf /etc/nginx/conf.d/groceries.conf
+sudo cp /home/brig/dev/groceries/deploy/nginx.conf /etc/nginx/conf.d/groceries.conf
 
 sudo nginx -t
 sudo systemctl reload nginx
